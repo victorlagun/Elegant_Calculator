697f46b3af64148f4d266ffecc5e055787da8387
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2022-12-04 14:13:53 +0300

    Add view binding

--- app/src/main/java/com/example/elegant/calculator/MainActivityKt.kt
+++ app/src/main/java/com/example/elegant/calculator/MainActivityKt.kt
@@ -2,10 +2,13 @@ package com.example.elegant.calculator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.elegant.calculator.databinding.ActivityMainBinding
 
 class MainActivityKt: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
