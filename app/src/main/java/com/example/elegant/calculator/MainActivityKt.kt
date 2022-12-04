900a9c124052387af7b8d42de286a6699a1d70d5
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2022-12-04 14:46:48 +0300

    Rewrite numeric buttons

--- app/src/main/java/com/example/elegant/calculator/MainActivityKt.kt
+++ app/src/main/java/com/example/elegant/calculator/MainActivityKt.kt
@@ -1,5 +1,6 @@
 package com.example.elegant.calculator
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.TypedValue
 import androidx.appcompat.app.AppCompatActivity
@@ -17,10 +18,64 @@ class MainActivityKt : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.apply {
+            button1.setOnClickListener {
+                ifErrorOnOutput()
+                exceedLength()
+                input.text = input.text.toString() + "1"
+            }
+            button2.setOnClickListener {
+                ifErrorOnOutput()
+                exceedLength()
+                input.text = input.text.toString() + "2"
+            }
+            button3.setOnClickListener {
+                ifErrorOnOutput()
+                exceedLength()
+                input.text = input.text.toString() + "3"
+            }
+            button4.setOnClickListener {
+                ifErrorOnOutput()
+                exceedLength()
+                input.text = input.text.toString() + "4"
+            }
+            button5.setOnClickListener {
+                ifErrorOnOutput()
+                exceedLength()
+                input.text = input.text.toString() + "5"
+            }
+            button6.setOnClickListener {
+                ifErrorOnOutput()
+                exceedLength()
+                input.text = input.text.toString() + "6"
+            }
+            button7.setOnClickListener {
+                ifErrorOnOutput()
+                exceedLength()
+                input.text = input.text.toString() + "7"
+            }
+            button8.setOnClickListener {
+                ifErrorOnOutput()
+                exceedLength()
+                input.text = input.text.toString() + "8"
+            }
+            button9.setOnClickListener {
+                ifErrorOnOutput()
+                exceedLength()
+                input.text = input.text.toString() + "9"
+            }
+            button0.setOnClickListener {
+                ifErrorOnOutput()
+                exceedLength()
+                input.text = input.text.toString() + "0"
+            }
+        }
     }
 
     private fun ifErrorOnOutput() {
