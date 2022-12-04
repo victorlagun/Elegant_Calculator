44bd0d5275f5fe0cf14fd8c53ce37edd21d2b909
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2022-12-04 14:10:40 +0300

    Create MainActivityKt

---
+++ app/src/main/java/com/example/elegant/calculator/MainActivityKt.kt
@@ -0,0 +1,11 @@
+package com.example.elegant.calculator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivityKt: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
