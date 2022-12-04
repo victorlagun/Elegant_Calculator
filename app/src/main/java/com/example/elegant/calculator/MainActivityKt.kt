11aba734a7ec17a2681668e2032c8ea116918598
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2022-12-04 14:28:42 +0300

    Rewrite ifErrorOnOutput method

--- app/src/main/java/com/example/elegant/calculator/MainActivityKt.kt
+++ app/src/main/java/com/example/elegant/calculator/MainActivityKt.kt
@@ -14,9 +14,17 @@ private const val MODULUS = '%'
 
 class MainActivityKt: AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    private fun ifErrorOnOutput() {
+        if (binding.output.text.toString() == "Error") {
+            binding.output.text = ""
+        }
     }
 }
