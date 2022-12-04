44a253a2e575599f0006b629f7e7dfcd1e4ba010
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2022-12-04 14:19:21 +0300

    Rewrite constants

--- app/src/main/java/com/example/elegant/calculator/MainActivityKt.kt
+++ app/src/main/java/com/example/elegant/calculator/MainActivityKt.kt
@@ -4,6 +4,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.elegant.calculator.databinding.ActivityMainBinding
 
+private const val ADDITION = '+'
+private const val SUBTRACTION = '-'
+private const val MULTIPLICATION = '*'
+private const val DIVISION = '/'
+private const val EQU = '='
+private const val EXTRA = '@'
+private const val MODULUS = '%'
+
 class MainActivityKt: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
