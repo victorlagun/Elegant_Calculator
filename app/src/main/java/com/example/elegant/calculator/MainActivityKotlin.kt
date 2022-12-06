676666a43cf3c624b17ad26df22dbdb77d99380e
Егор Шкляревский <egor39546@gmail.com>
2022-12-06 13:48:39 +0300

    Rewrite multiplication button listener

--- app/src/main/java/com/example/elegant/calculator/MainActivityKotlin.kt
+++ app/src/main/java/com/example/elegant/calculator/MainActivityKotlin.kt
@@ -141,6 +141,21 @@ class MainActivityKotlin: AppCompatActivity() {
                 t2.text = "Error"
             }
         }
+
+        b_multi.setOnClickListener {
+            if (t1.text.isNotEmpty()) {
+                ACTION = MULTIPLICATION
+                operation()
+                if (!ifReallyDecimal()) {
+                    t2.text = "$val1×"
+                } else {
+                    t2.text = val1.toInt().toString() + "×"
+                }
+                t1.text = null
+            } else {
+                t2.text = "Error"
+            }
+        }
     }
 
     // Remove error message that is already written there.
