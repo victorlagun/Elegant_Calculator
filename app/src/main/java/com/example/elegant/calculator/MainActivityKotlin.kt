7ab4bbae26c9107f75ca2f7d739d6b2305242baf
Егор Шкляревский <egor39546@gmail.com>
2023-01-05 00:26:56 +0300

    Rewrite ifReallyDecimal method and add constants

--- app/src/main/java/com/example/elegant/calculator/MainActivityKotlin.kt
+++ app/src/main/java/com/example/elegant/calculator/MainActivityKotlin.kt
@@ -1,13 +1,23 @@
 package com.example.elegant.calculator
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.elegant.calculator.databinding.ActivityMainBinding
 
-@SuppressLint("SetTextI18n")
+const val ADDITION = '+'
+const val SUBTRACTION = '-'
+const val MULTIPLICATION = '*'
+const val DIVISION = '/'
+const val EQU = '='
+const val EXTRA = '@'
+const val MODULUS = '%'
+const val ACTION = 0.toChar()
+
 class MainActivityKotlin : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val val1 = Double.NaN
+    private val val2 = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +30,10 @@ class MainActivityKotlin : AppCompatActivity() {
         if ("${binding.output.text}" == "Error") {
             binding.output.text = ""
         }
+    }
+
+    // Whether value if a double or not
+    private fun ifReallyDecimal(): Boolean {
+        return val1 == val1.toInt().toDouble()
     }
 }
